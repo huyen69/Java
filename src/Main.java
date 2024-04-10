8be037fd0f9ae461c5a3a450764cf7ccd66bb3da
huyen69 <minhhuyen19122005chaob@gmail.com>
2024-04-10 21:51:18 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // main => ctrl + space
+     public static void main(String[] args) {
+         //sysout => ctrl + space => enter
+         System.out.println("Hello world");
+    }
+}
